fc9cc062986e535c8c408fa4ba79e42431bc050d
SatriaAPN <satrianusa10@gmail.com>
2022-07-21 07:43:53 +0700

    create endpoint to serve the static content

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const server = http.createServer(app);
 const path = require('path');
 const public = path.join(__dirname, 'public');
 
+app.use(express.static(public));
+
 server.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
