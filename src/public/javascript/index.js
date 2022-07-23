cd3d974754916821fbf557d65063ecf643ad3621
SatriaAPN <satrianusa10@gmail.com>
2022-07-24 02:35:21 +0700

    create filteredQuerySyntaxt in string filtering

--- src/public/javascript/index.js
+++ src/public/javascript/index.js
@@ -3,3 +3,5 @@ function textToHTML() {
   const beautifiedQuery = beautifyQuery(query);
   document.getElementById("demo").innerHTML = beautifiedQuery;
 }
+
+const filteredQuerySyntaxt = ["select", "from", "where", "group by", "order by", "limit", "as", "when", "and", "then", "or", "else", "left", "join", "right", "is", "null", "false", "true"];
