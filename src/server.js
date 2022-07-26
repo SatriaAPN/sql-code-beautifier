fb61953c8230651fb7264a7aa2dda693271edd4f
SatriaAPN <satrianusa10@gmail.com>
2022-07-26 09:27:22 +0700

    add endpoint for sql beautifier at 'localhost:3000/'

--- src/server.js
+++ src/server.js
@@ -7,6 +7,14 @@ const public = path.join(__dirname, 'public');
 
 app.use(express.static(public));
 
+app.get('/', (req, res) => {
+  try {
+    res.sendFile(public + '/index.html');
+  } catch(e) {
+    res.status(500).send(e);
+  }
+})
+
 server.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
